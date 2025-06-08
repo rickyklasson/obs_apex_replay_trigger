4fcdedeadd3d1eb80e536292c329fe5fd3838236
Ricky Klasson <mrklasson@gmail.com>
2025-06-08 13:27:52 +0200

    Minor changes

--- manager.py
+++ manager.py
@@ -14,7 +14,7 @@ import time
 
 
 class Manager:
-    TRIGGER_DELAY = 10  # Delay to wait for further events before saving replay buffer.
+    TRIGGER_DELAY = 15  # Delay to wait for further events before saving replay buffer.
 
     def __init__(self, websocket_port: int, websocket_pass: str):
         self.obs_client = obs.ReqClient(host='localhost', port=websocket_port, password=websocket_pass, timeout=3)
@@ -30,7 +30,7 @@ class Manager:
         return last_file
 
     def monitor_replay_triggers(self, triggers: list[str]):
-        PERIOD = 0.5
+        PERIOD = 0.75
         replay_triggers = [events.EventType.from_str(s) for s in triggers]
 
         try:
@@ -48,12 +48,11 @@ class Manager:
                 event = events.detect_event(img)
 
                 if event is not None:
-                    logging.info(f'Game event: {event} detected at {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+                    logging.debug(f'Game event: {event} detected at {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
 
                     if event.type in replay_triggers:
                         last_event_time = time.time()
                         logging.debug(f'Last event time: {last_event_time:.1f}')
-                        break
 
                 if last_event_time is not None and time.time() - last_event_time > self.TRIGGER_DELAY:
                     logging.info(f'Event delay passed. Saving replay buffer!')
@@ -62,7 +61,6 @@ class Manager:
 
                 # Sleep.
                 end_time = time.time()
-                logging.info(f'Ran in: {end_time - start_time:.3f}s', end='\r', flush=True)
                 time.sleep(max(0, PERIOD - (end_time - start_time)))
 
         except KeyboardInterrupt:
@@ -124,8 +122,9 @@ class Manager:
 
                 # Sleep.
                 end_time = time.time()
-                logging.debug(f'({state}) Ran in: {end_time - start_time:.3f}s', end='\r', flush=True)
+                logging.debug(f'({state}) Ran in: {end_time - start_time:.3f}s')
                 time.sleep(max(0, PERIOD - (end_time - start_time)))
+
         except KeyboardInterrupt:
             pass
         finally:
